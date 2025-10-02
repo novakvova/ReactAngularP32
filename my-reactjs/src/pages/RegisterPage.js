4d0c758de875361e86576483f67f1e14f9ea3609
novakvova <novakvova@gmail.com>
2025-10-02 17:33:36 +0300

    Example props

--- my-reactjs/src/pages/RegisterPage.js
+++ my-reactjs/src/pages/RegisterPage.js
@@ -5,30 +5,50 @@ const RegisterPage = () => {
 
     const [form, setForm] = useState({
         email: "",
-        password: "",
+        firstName: "",
+        age: 18,
     });
 
     const onChangeHandler = (e) => {
-        console.log("On change handler", e);
+        // console.log("On change handler", e.target.value);
+        // console.log("On change handler", e.target.name);
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Data", form);
     }
 
     return (
         <>
             <h1 className={"text-center"}>Реєстрація</h1>
             <div className="row">
-                <form className={"col-md-6 offset-md-3"}>
+                <form onSubmit={onSubmit} className={"col-md-6 offset-md-3"}>
                     <TextFieldBase
                         label={"Електронна пошта"}
-                        field={"email"}
+                        field={`email`}
                         type={"email"}
+                        onChange={onChangeHandler}
                         value={form.email}
                     />
 
                     <TextFieldBase
                         label={"Прізвище"}
                         field={"firstName"}
-                        value={form.password}
+                        onChange={onChangeHandler}
+                        value={form.firstName}
                     />
+
+                    <TextFieldBase
+                        label={"Вік"}
+                        field={"age"}
+                        onChange={onChangeHandler}
+                        type={"number"}
+                        value={form.age}
+                    />
+
+                    <button className={"btn btn-success"} type={"submit"}>Реєстрація</button>
                 </form>
             </div>
         </>
