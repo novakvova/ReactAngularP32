66a58ef51c02e8b9980d9d298b54d23d23911a9d
novakvova <novakvova@gmail.com>
2025-10-16 16:41:27 +0300

    View list categories

--- my-reactaxios/src/App.js
+++ my-reactaxios/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useEffect, useState} from "react";
 import axiosInstance from "./api/axiosInstance";
+import {API_URL} from "./api/apiConfig";
 
 function App() {
 
@@ -16,20 +17,53 @@ function App() {
             const response = await axiosInstance.get("/categories/list");
             //console.log("Result request", response.data);
             setCategories(response.data);
-        }
-        catch (error) {
+        } catch (error) {
             console.error("Помилка запиту на сервер", error)
         }
     }
 
     return (
         <>
-            <h1>Привіт React</h1>
-            <ul>
-                {categories.map((category) => (
-                    <li key={category.id}>{category.title}</li>
-                ))}
-            </ul>
+            <h1 className={"text-center text-4xl font-normal text-gray-900 dark:text-white"}>
+                Привіт React
+            </h1>
+
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Фото
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Назва
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    {categories.map((category) => (
+                        <tr key={category.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                            <td className="px-6 py-4">
+                                {category.image  ?
+                                    <img src={`${API_URL}/images/200_${category.image}`}
+                                         alt={category.title} width={100}/>
+                                    :
+                                    <img src="https://t4.ftcdn.net/jpg/05/97/47/95/360_F_597479556_7bbQ7t4Z8k3xbAloHFHVdZIizWK1PdOo.jpg" alt={category.title} width={100}/>
+                                }
+
+                            </td>
+                            <th scope="row"
+                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {category.title}
+                            </th>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+
         </>
     );
 }
