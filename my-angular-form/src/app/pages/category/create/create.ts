51de8fae8e78462641f077545f2c7e8ef24d6ca5
novakvova <novakvova@gmail.com>
2025-10-21 16:16:59 +0300

    Begin working form

--- my-angular-form/src/app/pages/category/create/create.ts
+++ my-angular-form/src/app/pages/category/create/create.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {CategoryFormTemplate} from '../../../components/category-form-template/category-form-template';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [
+    CategoryFormTemplate
+  ],
   templateUrl: './create.html',
   styleUrl: './create.css'
 })
