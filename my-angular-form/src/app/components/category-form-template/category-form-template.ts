9d97b39cfa07eff971b701ff30817f80dd1dde4e
novakvova <novakvova@gmail.com>
2025-10-23 17:00:40 +0300

    Working app is good view form

--- my-angular-form/src/app/components/category-form-template/category-form-template.ts
+++ my-angular-form/src/app/components/category-form-template/category-form-template.ts
@@ -1,8 +1,15 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors} from '@angular/forms';
+import {KeyValuePipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-category-form-template',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf,
+    NgForOf,
+    KeyValuePipe
+  ],
   templateUrl: './category-form-template.html',
   styleUrl: './category-form-template.css'
 })
@@ -13,9 +20,31 @@ export class CategoryFormTemplate implements OnInit {
   //Цей клас приймає у себе параметри
   @Input() title: string = 'Create Category';
 
+  @Output() formSubmit =
+    new EventEmitter<FormGroup>();
+
+  //Форма для категорії, потрібно надати значення
+  categoryForm: FormGroup;
+  errorMessage: string | null = null;
+
+  //FormBuilder - клас для побудови форм
+  constructor(private fb: FormBuilder) {
+    this.categoryForm = this.fb.group({
+      title: ['', this.requiredMessage('Назва є обов\'язковою')],
+      urlSlug: [''],
+      image: null
+    })
+  }
+
   //Коли буде виконуватися редагування категорії
   ngOnInit(): void {
     console.log("Ініціалізація об'єкта", "Можу послати запит на севрер")
+  }
+
+  requiredMessage(message: string) {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return control.value ? null : { required: message };
+    }
   }
 
 }
--- my-angular-form/src/app/pages/category/create/create.ts
+++ my-angular-form/src/app/pages/category/create/create.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {CategoryFormTemplate} from '../../../components/category-form-template/category-form-template';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -11,4 +12,10 @@ import {CategoryFormTemplate} from '../../../components/category-form-template/c
 })
 export class CategoryCreate {
 
+  onSubmit(form: FormGroup) {
+    if(form.invalid) {
+      return;
+    }
+    console.log("Submit form", form.value);
+  }
 }
